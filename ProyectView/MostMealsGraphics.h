b0a5f57c32745dad97dbb8869247692212bb6de5
Joel <joel.espinoza@pucp.edu.pe>
2023-03-03 15:07:57 -0500

    Grafico de Meals

--- ProyectView/MostMealsGraphics.h
+++ ProyectView/MostMealsGraphics.h
@@ -41,10 +41,11 @@ namespace ProyectView {
 			}
 		}
 	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chartCommonMeals;
-	private: System::Windows::Forms::DateTimePicker^ dateTimePickerMeals;
+	private: System::Windows::Forms::Label^ label1;
+
 	protected:
 
-	private: System::Windows::Forms::Button^ button1;
+
 
 	private:
 		/// <summary>
@@ -63,8 +64,7 @@ namespace ProyectView {
 			System::Windows::Forms::DataVisualization::Charting::Legend^ legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
 			System::Windows::Forms::DataVisualization::Charting::Series^ series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
 			this->chartCommonMeals = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
-			this->dateTimePickerMeals = (gcnew System::Windows::Forms::DateTimePicker());
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chartCommonMeals))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -74,70 +74,61 @@ namespace ProyectView {
 			this->chartCommonMeals->ChartAreas->Add(chartArea1);
 			legend1->Name = L"Legend1";
 			this->chartCommonMeals->Legends->Add(legend1);
-			this->chartCommonMeals->Location = System::Drawing::Point(120, 145);
+			this->chartCommonMeals->Location = System::Drawing::Point(47, 132);
 			this->chartCommonMeals->Name = L"chartCommonMeals";
 			series1->ChartArea = L"ChartArea1";
 			series1->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Pie;
 			series1->Legend = L"Legend1";
 			series1->Name = L"Platos";
 			this->chartCommonMeals->Series->Add(series1);
-			this->chartCommonMeals->Size = System::Drawing::Size(523, 432);
+			this->chartCommonMeals->Size = System::Drawing::Size(479, 405);
 			this->chartCommonMeals->TabIndex = 2;
 			this->chartCommonMeals->Text = L"chart1";
 			// 
-			// dateTimePickerMeals
+			// label1
 			// 
-			this->dateTimePickerMeals->Location = System::Drawing::Point(141, 55);
-			this->dateTimePickerMeals->Name = L"dateTimePickerMeals";
-			this->dateTimePickerMeals->Size = System::Drawing::Size(299, 22);
-			this->dateTimePickerMeals->TabIndex = 3;
-			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(501, 55);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(142, 37);
-			this->button1->TabIndex = 4;
-			this->button1->Text = L"Seleccionar Fecha";
-			this->button1->UseVisualStyleBackColor = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 19.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(130, 44);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(378, 47);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"PLATOS VENDIDOS";
 			// 
 			// MostMealsGraphics
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(916, 655);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->dateTimePickerMeals);
+			this->ClientSize = System::Drawing::Size(646, 593);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->chartCommonMeals);
 			this->Name = L"MostMealsGraphics";
 			this->Text = L"MostMealsGraphics";
-			this->Load += gcnew System::EventHandler(this, &MostMealsGraphics::button1_Click);
+			this->Load += gcnew System::EventHandler(this, &MostMealsGraphics::MostMealsGraphics_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chartCommonMeals))->EndInit();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 		void RefreshDGV() {
-			DateTime dateTime = dateTimePickerMeals->Value;
-			String^ FechaMeals = dateTime.ToString("yyyy-MM-dd");
-
+			//DateTime dateTime = dateTimePickerMeals->Value;
+			//String^ FechaMeals = dateTime.ToString("yyyy-MM-dd");
 
 			chartCommonMeals->Series["Platos"]->Points->Clear();
+			//int k = 0;
 
-
-			int k = 0;
 			if (chartCommonMeals != nullptr) {
 				List<Meals^>^ MealList = Controller::QueryAllMeals();
+
 				for (int i = 0; i < MealList->Count; i++) {
 
-					if (MealList[i]->DateMeal->Contains(FechaMeals)) {
-						chartCommonMeals->Series["Platos"]->Points->Add(MealList[i]->StockUsed);
-						if (k < chartCommonMeals->Series["Platos"]->Points->Count) {
-							chartCommonMeals->Series["Platos"]->Points[k]->LegendText = MealList[i]->Name;
-							chartCommonMeals->Series["Platos"]->Points[k]->Label = Convert::ToString(MealList[i]->StockUsed);
-						}
-						k++;
-					}
+					//if (MealList[i]->DateMeal->Contains(FechaMeals)) {
+					chartCommonMeals->Series["Platos"]->Points->Add(MealList[i]->StockUsed);
+					chartCommonMeals->Series["Platos"]->Points[i]->LegendText = MealList[i]->Name;
+					chartCommonMeals->Series["Platos"]->Points[i]->Label = Convert::ToString(MealList[i]->StockUsed);
+					//}
+					//k++;
+
 				}
 			}
 
@@ -145,9 +136,12 @@ namespace ProyectView {
 
 
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		RefreshDGV();
+		
 	}
 
 
-	};
+	private: System::Void MostMealsGraphics_Load(System::Object^ sender, System::EventArgs^ e) {
+		RefreshDGV();
+	}
+};
 }
